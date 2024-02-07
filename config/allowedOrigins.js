8f8e149022dc58f99231493c0b04fd1577dcfd12
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-02-06 20:27:27 -0600

    Second Commit

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     //'http://localhost:3000/dash/users',
-    //'https://technotes-bgv4.onrender.com',
+    'https://technotes-bgv4.onrender.com',
     //'https://dandrepairshop.com'
+    'https://technotes-api.onrender.com/notes'
 ]
 
 module.exports = allowedOrigins

efd4416e99adcc5a4bff3eca89c89172fac0c33c
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-02-09 11:04:39 -0600

    Fourth Commit

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ connectDB()
 
 app.use(logger)
 
-app.use(cors(corsOptions))
-// app.use(cors({
-//     origin: "*",
-// }))
+// app.use(cors(corsOptions))
+app.use(cors({
+    origin: "*",
+}))
 
 //app.use(cors())
    

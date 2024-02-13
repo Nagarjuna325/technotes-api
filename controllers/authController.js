6aeb33eabdc204191d01f7aca3e44a521134c516
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-02-13 00:16:47 -0600

    Sixth Commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,8 +14,9 @@ const login = asyncHandler(async (req, res) => {
     }
 
     const foundUser = await User.findOne({ username }).exec()
-
+    //console.log(foundUser)
     if (!foundUser || !foundUser.active) {
+        
         return res.status(401).json({ message: 'Unauthorized' })
     }
 

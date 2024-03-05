d2265f422cb4cc9e6e76689f83c3763badcd4b25
Nagarjuna325 <nagarjunreddybolla@gmail.com>
2024-03-05 11:16:21 -0600

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 const usersController = require('../controllers/usersController')
 
-// const verifyJWT = require('../middleware/verifyJWT')
+const verifyJWT = require('../middleware/verifyJWT')
 
-// router.use(verifyJWT)
+router.use(verifyJWT)
 
 router.route('/')
     .get(usersController.getAllUsers)
